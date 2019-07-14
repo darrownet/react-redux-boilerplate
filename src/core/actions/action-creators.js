1685876dc85173e1a0e8d4de3234476e0ca4ecd6
Matt Schiffman <matt.s@bigmirrorlabs.com>
2019-07-14 12:11:27 -0400

    formatting

--- src/core/actions/action-creators.js
+++ src/core/actions/action-creators.js
@@ -1,16 +1,19 @@
 import * as types from './action-types.js';
 
 export let actions = {
+
   addItem: function (value) {
     return {
       type: types.ADD_ITEM,
       value
     }
   },
+
   deleteItem: function (value) {
     return {
       type: types.DELETE_ITEM,
       value
     }
   }
+
 };
